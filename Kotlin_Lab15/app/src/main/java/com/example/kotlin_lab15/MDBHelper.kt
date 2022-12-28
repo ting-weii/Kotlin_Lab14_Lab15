1b0473627373906e2077e5613d5097f6a015503f
ting-weii <ptsoho.tine@gmail.com>
2022-12-28 18:58:56 +0800

    2

--- Kotlin_Lab15/app/src/main/java/com/example/kotlin_lab15/MDBHelper.kt
+++ Kotlin_Lab15/app/src/main/java/com/example/kotlin_lab15/MDBHelper.kt
@@ -3,7 +3,6 @@ package com.example.kotlin_lab15
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-//自訂建構子並繼承 SQLiteOpenHelper 類別
 class MyDBHelper (
     context: Context,
     name: String = database,
@@ -11,16 +10,14 @@ class MyDBHelper (
     version: Int = v
 ) : SQLiteOpenHelper(context, name, factory, version) {
     companion object {
-        private const val database = "myDatabase" //資料庫名稱
-        private const val v = 1 //資料庫版本
+        private const val database = "myDatabase"
+        private const val v = 1
     }
     override fun onCreate(db: SQLiteDatabase) {
-        //建立 myTable 資料表，表內有 book 字串欄位和 price 整數欄位
         db.execSQL("CREATE TABLE myTable(book text PRIMARY KEY, price integer NOT NULL)")
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int,
                            newVersion: Int) {
-        //升級資料庫版本時，刪除舊資料表，並重新執行 onCreate()，建立新資料表
         db.execSQL("DROP TABLE IF EXISTS myTable")
         onCreate(db)
     }
--- Kotlin_Lab15/app/src/main/java/com/example/kotlin_lab15/MainActivity.kt
+++ Kotlin_Lab15/app/src/main/java/com/example/kotlin_lab15/MainActivity.kt
@@ -13,30 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //取得資料庫實體
         dbrw = MyDBHelper(this).writableDatabase
-        //宣告 Adapter 並連結 ListView
         adapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_1, items)
         findViewById<ListView>(R.id.listView).adapter = adapter
-        //設定監聽器
         setListener()
     }
     override fun onDestroy() {
-        dbrw.close() //關閉資料庫
+        dbrw.close()
         super.onDestroy()
     }
-    //設定監聽器
     private fun setListener() {
         val ed_book = findViewById<EditText>(R.id.ed_book)
         val ed_price = findViewById<EditText>(R.id.ed_price)
         findViewById<Button>(R.id.btn_insert).setOnClickListener {
-            //判斷是否有填入書名或價格
             if (ed_book.length() < 1 || ed_price.length() < 1)
                 showToast("欄位請勿留空")
             else
                 try {
-                    //新增一筆書籍紀錄於 myTable 資料表
                     dbrw.execSQL(
                         "INSERT INTO myTable(book, price) VALUES(?,?)",
                         arrayOf(ed_book.text.toString(),
@@ -49,12 +43,10 @@ class MainActivity : AppCompatActivity() {
                 }
         }
         findViewById<Button>(R.id.btn_update).setOnClickListener {
-            //判斷是否有填入書名或價格
             if (ed_book.length() < 1 || ed_price.length() < 1)
                 showToast("欄位請勿留空")
             else
                 try {
-                    //尋找相同書名的紀錄並更新 price 欄位的值
                     dbrw.execSQL("UPDATE myTable SET price = ${ed_price.text} WHERE book LIKE '${ed_book.text}'")
                     showToast("更新:${ed_book.text},價格:${ed_price.text}")
                     cleanEditText()
@@ -63,12 +55,10 @@ class MainActivity : AppCompatActivity() {
                 }
         }
         findViewById<Button>(R.id.btn_delete).setOnClickListener {
-            //判斷是否有填入書名
             if (ed_book.length() < 1)
                 showToast("書名請勿留空")
             else
                 try {
-                    //從 myTable 資料表刪除相同書名的紀錄
                     dbrw.execSQL("DELETE FROM myTable WHERE book LIKE '${ed_book.text}'")
                     showToast("刪除:${ed_book.text}")
                     cleanEditText()
@@ -77,28 +67,25 @@ class MainActivity : AppCompatActivity() {
                 }
         }
         findViewById<Button>(R.id.btn_query).setOnClickListener {
-            //若無輸入書名則 SQL 語法為查詢全部書籍，反之查詢該書名資料
             val queryString = if (ed_book.length() < 1)
                 "SELECT * FROM myTable"
             else
                 "SELECT * FROM myTable WHERE book LIKE '${ed_book.text}'"
             val c = dbrw.rawQuery(queryString, null)
-            c.moveToFirst() //從第一筆開始輸出
-            items.clear() //清空舊資料
+            c.moveToFirst()
+            items.clear()
             showToast("共有${c.count}筆資料")
             for (i in 0 until c.count) {
-                //加入新資料
                 items.add("書名:${c.getString(0)}\t\t\t\t 價格:${c.getInt(1)}")
-                c.moveToNext() //移動到下一筆
+                c.moveToNext()
             }
-            adapter.notifyDataSetChanged() //更新列表資料
-            c.close() //關閉 Cursor
+            adapter.notifyDataSetChanged()
+            c.close()
         }
     }
-    //建立 showToast 方法顯示 Toast 訊息
     private fun showToast(text: String) =
         Toast.makeText(this,text, Toast.LENGTH_LONG).show()
-    //清空輸入的書名與價格
+
     private fun cleanEditText() {
         findViewById<EditText>(R.id.ed_book).setText("")
         findViewById<EditText>(R.id.ed_price).setText("")
